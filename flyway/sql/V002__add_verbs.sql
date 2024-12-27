5c9efe08870de2774e53023a86b805198f023d94
Alessandro <alex.spasiano@gmail.com>
2024-12-27 15:30:23 +0100

    feat: added some irregular verbs

---
+++ flyway/sql/V002__add_verbs.sql
@@ -0,0 +1,15 @@
+INSERT INTO irregular_verbs (base_form, past_simple, past_participle)
+VALUES
+('be', 'was/were', 'been'),
+('become', 'became', 'become'),
+('begin', 'began', 'begun'),
+('break', 'broke', 'broken'),
+('bring', 'brought', 'brought'),
+('build', 'built', 'built'),
+('buy', 'bought', 'bought'),
+('catch', 'caught', 'caught'),
+('choose', 'chose', 'chosen'),
+('come', 'came', 'come'),
+('cost', 'cost', 'cost'),
+('cut', 'cut', 'cut'),
+('do', 'did', 'done');
